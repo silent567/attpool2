21115ce70f5bcd012c9c2d062480354f01fa5e90
Hao Tang <tanghaosjtu@gmail.com>
2020-05-07 00:15:26 +0000

    Initial DimeNet branch

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-# from .torch_attention import *
+from .torch_attention import FastFlexAddAttention, MultipleAttention
+from .torch_mapping import Gfusedmax, Sparsemax
--- my_test.py
+++ my_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from torch_mapping import Gfusedmax, Sparsemax, Softmax
-from torch_attention import FastFlexAddAttention, MultipleAttention
+from .torch_mapping import Gfusedmax, Sparsemax, Softmax
+from .torch_attention import FastFlexAddAttention, MultipleAttention
 import numpy as np
 import torch
 
